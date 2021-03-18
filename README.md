96cb13e19939b5992ef42ffcbb1b75f8f2f35efc
El Roble <san_lucas2005@yahoo.com>
2021-03-18 14:04:32 -0700

    Changes in README.md and comments added to HTML, CSS and JS files.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# jorgesoto-homework-march-23-21
+# jorgesoto-homework-march-23-2021
+
+# Password Generator Project
+The goal of this project is to create a strong password that provides greater security, for this reason it was necessary to accomplish some conditions:
+
+* Password length of at least 8 characters and no more than 128 characters.
+* Character types to include in the password:
+    * lowercase, uppercase, numeric, and/or special characters.
+* Input should be validated and at least one character type should be selected.
+
+After the conditions above are accomplished a password is generated and it is displayed in the page.
---
+++ js/script.js
@@ -0,0 +1,121 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate")
+const minPasswordLength = 8
+const maxPasswordLength = 128
+
+//Array For Storing Character Types Selections
+var options = []
+
+// Write password to the #password input
+function writePassword() {
+  options = []
+  var userChoicePasswordLength = 0
+
+  //Asking user for password length
+  userChoicePasswordLength = prompt("Length of password (8-128):")
+
+  // In case of user selects cancel, the program ends.
+  if (userChoicePasswordLength === null) {
+    return
+  }
+
+  /* 
+  In case of user inputs a length, It is going to verify
+  the min and max length.
+  */
+  while (
+    userChoicePasswordLength < minPasswordLength ||
+    userChoicePasswordLength > maxPasswordLength
+  ) {
+    /*If the length is not accomplished, It is going 
+to be asking to input the length again.
+*/
+    userChoicePasswordLength = prompt("Length of password (8-128):")
+
+    // In case of user selects cancel, the program ends.
+    if (userChoicePasswordLength === null) {
+      return
+    }
+  }
+
+  /*
+If the user not select at least on character type, 
+the program is going to be asking to do it.
+*/
+  while (options.length == 0) {
+    if (confirm("lowercase?")) {
+      options.push(0)
+    }
+
+    if (confirm("uppercase?")) {
+      options.push(1)
+    }
+
+    if (confirm("numeric?")) {
+      options.push(2)
+    }
+
+    if (confirm("special?")) {
+      options.push(3)
+    }
+    if (options.length == 0) {
+      alert("Choose at least one character type...")
+    }
+  }
+
+  /*
+If the above conditions are accomplished, 
+It is called the function for generating the password,
+and shown it on screen.
+*/
+  var password = generatePassword(options, userChoicePasswordLength)
+  var passwordText = document.querySelector("#password")
+  passwordText.value = password
+}
+
+function generatePassword(Options, UserChoicePasswordLength) {
+  /*
+This function receives the character types selected 
+and the password length.
+Options and UserChoicePasswordLength
+*/
+
+  /*
+Are defined some constants with the character types content.
+*/
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz"
+  const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  const numericChars = "0123456789"
+  const specialChars = "!”#$%&’()*+,-./:;<=>?@[]^_`{|}~"
+
+  //One constant stores the whole character types in the order was selected.
+  const allChars = [lowercaseChars, uppercaseChars, numericChars, specialChars]
+
+  //Variable that holds the group of character types selected.
+  var passwordBaseChar = ""
+
+  
+  //Loop for filling the variable that holds the character types group selected, 
+  for (var i = 0; i < options.length; i++) {
+    passwordBaseChar += allChars[Options[i]]
+  }
+
+  let randomNumber = 0
+  let passwordChar = ""
+  let passwordGenerated = ""
+
+  /*
+  Loop that iterate the password length input, 
+  and generating the password randomly from the character types group created.
+  */
+  for (var i = 1; i <= UserChoicePasswordLength; i++) {
+    randomNumber = Math.floor(Math.random() * passwordBaseChar.length)
+    passwordChar = passwordBaseChar[randomNumber]
+    passwordGenerated += passwordChar
+  }
+
+  return passwordGenerated
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword)
