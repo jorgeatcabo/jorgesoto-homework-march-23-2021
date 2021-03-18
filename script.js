6e47ae47956bdb971a5a2dc79b96ac7f9aaf9613
El Roble <san_lucas2005@yahoo.com>
2021-03-17 18:15:33 -0700

    First test online

--- script.js
+++ script.js
@@ -1,45 +1,69 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-let options=[];
-options=[0];
+var options=[];
+
+
+  const minPasswordLength=8;
+  var userChoicePasswordLength=16;
+  
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  options=[];
+  
+  if (confirm("lowercase?")) {
+    options.push(0);  
+  }
+  
+  if (confirm("uppercase?")) {
+    options.push(1);  
+  }
 
-  passwordText.value = password;
+  if (confirm("numeric?")) {
+    options.push(2);  
+  }
+
+  if (confirm("special?")) {
+    options.push(3);  
+  }
+  
+    var password = generatePassword(options,userChoicePasswordLength);
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+  
+    // passwordText.value = "You didn't select any character types...";
+  
 
 }
 
-
-
-
-function generatePassword(options){
+function generatePassword(Options,UserChoicePasswordLength){
   const lowercaseChars="abcdefghijklmnopqrstuvwxyz";
   const uppercaseChars="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const numericChars="0123456789";
   const specialChars="!”#$%&’()*+,-./:;<=>?@[\]^_`{|}~";
-  const allChars=[lowercaseChars,uppercaseChars,specialChars];
-  const minPasswordLength=8;
-  let userChoicePasswordLength=8;
+  const allChars=[lowercaseChars,uppercaseChars,numericChars,specialChars];
   
-  // let isUserChoiceLowercaseChars=true;
-  // let isUserChoiceUppercaseChars=false;
-  // let isUserChoiceSpecialChars=true;
+  var passwordBaseChar="";
 
- var charBasePassword="";
- for (var i=0; i<options.length; i++){      
-  charBasePassword+=allChars[options[i]];
- }
+  for (var i=0; i<options.length; i++){      
+    passwordBaseChar+=allChars[Options[i]];
+  }
 
- console.log(charBasePassword);
+  let randomNumber=0;
+  let passwordChar="";
+  let passwordGenerated="";
 
-  
-  //return password;
+  for(var i=1; i<=UserChoicePasswordLength; i++){
+    // console.log(Math.floor(Math.random()*passwordBaseChar.length));
+    randomNumber=Math.floor(Math.random()*passwordBaseChar.length);
+    passwordChar=passwordBaseChar[randomNumber];
+    passwordGenerated+=passwordChar;
+  }
+  return passwordGenerated;
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
-generatePassword(options);
+  generateBtn.addEventListener("click", writePassword);
